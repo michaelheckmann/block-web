0197c499c2f7096dbce14d468046f31071846f63
michael_heckmann <michael.heckmann@qart.shop>
2022-09-28 09:16:56 +0200

    trigger rebuild

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,10 +5,10 @@ import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
-
-import { Toaster } from '@redwoodjs/web/toast'
 import './index.css'
 import './scaffold.css'
+
+import { Toaster } from '@redwoodjs/web/toast'
 
 const App = () => (
   <FatalErrorBoundary page={FatalErrorPage}>
